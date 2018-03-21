e980b26791cf02fe9e205a3c2edbdb172a39babc
Lenny Lopes <lennymlopes@gmail.com>
2018-03-21 20:42:05 +0100

    add database config

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
-app.config.from_object(Config)							# import configurations from config.py
+app.config.from_object(Config)			# import configurations from config.py
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-from app import routes
+from app import routes, models
