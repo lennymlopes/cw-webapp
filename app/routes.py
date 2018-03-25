15be02108cb85319561a591b3b876f7cdad49386
Lenny Lopes <lennymlopes@gmail.com>
2018-03-25 19:31:04 +0200

    cosmetic changes in flash message

--- app/routes.py
+++ app/routes.py
@@ -58,7 +58,7 @@ def new():
 		alarm = Alarm(hour=form.hours.data, minute=form.minutes.data, repeat=form.repeat.data, label=form.label.data)
 		db.session.add(alarm)
 		db.session.commit()
-		flash('New alarm "{}"added at {}: {}, on '.format(
+		flash('New alarm "{}"added at {}:{}, on '.format(
 			form.label.data, form.hours.data, form.minutes.data))
 		return redirect(url_for('index'))
 	return render_template('alarm.html', title='Add Alarms', form=form)
