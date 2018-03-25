ffaeee00c4b75ee7180cf414eb03d90d92312dbf
Lenny Lopes <lennymlopes@gmail.com>
2018-03-25 11:50:03 +0200

    add database for alarms and users

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
@@ -9,12 +10,19 @@ class User(db.Model):
 	def __repr__(self):
 		return '<User {}>'.format(self.username)
 
+	def set_password(self, password):
+		self.password_hash = generate_password_hash(password)
+
+	def check_password(self, password):
+		return check_password_hash(self.password_hash, password)
+
 class Alarm(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	time = db.Column(db.String(64), index=True, unique=True)
 	active = db.Column(db.Boolean, index=True, unique=True)
 	repeat = db.Column(db.Integer)
 	label = db.Column(db.String(64))
-
+	duration = db.Column(db.Integer)
+	
 	def __repr__(self):
 		return '<Alarm {}>'.format(self.id)
