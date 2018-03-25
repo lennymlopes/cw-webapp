95dba26cfa8157a01e66021c1a12fa2489f7ac79
Lenny Lopes <lennymlopes@gmail.com>
2018-03-25 15:32:29 +0200

    add login/logout functionality

--- app/models.py
+++ app/models.py
@@ -1,7 +1,8 @@
-from app import db
+from app import db, login
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
-class User(db.Model):
+class User(UserMixin, db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	username = db.Column(db.String(64), index=True, unique=True)
 	email = db.Column(db.String(120), index=True, unique=True)
@@ -26,3 +27,8 @@ class Alarm(db.Model):
 	
 	def __repr__(self):
 		return '<Alarm {}>'.format(self.id)
+
+
+@login.user_loader
+def load_user(id):
+	return User.query.get(int(id))
--- webapp.py
+++ webapp.py
@@ -1,7 +1,7 @@
-from app import app
+from app import app, db
 
 from app.models import User, Alarm
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User, 'Alarm': Alarm}
+	return {'db': db, 'User': User, 'Alarm': Alarm}
