4988d48f73f58e8ffb867bf43a42562b4e428c0c
Lenny Lopes <lennymlopes@gmail.com>
2018-03-26 08:30:00 +0200

    show active days, add collapsible settings

--- app/models.py
+++ app/models.py
@@ -20,9 +20,9 @@ class User(UserMixin, db.Model):
 class Alarm(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	#time = db.Column(db.String(64), index=True, unique=True)
-	hour = db.Column(db.Integer)
-	minute = db.Column(db.Integer)
-	active = db.Column(db.Boolean, index=True, unique=True)
+	hour = db.Column(db.Integer, index=True)
+	minute = db.Column(db.Integer, index=True)
+	#active = db.Column(db.Boolean, default=True)
 	repeat = db.Column(db.String(64))
 	label = db.Column(db.String(64))
 	duration = db.Column(db.Integer)
