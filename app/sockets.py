e9469d9ee3e3fca07a3bf0097b9a233dfc788bb4
Lenny Lopes <lennymlopes@gmail.com>
2018-04-22 15:42:32 +0200

    add websocket functionality

--- app/sockets.py
+++ app/sockets.py
@@ -1,4 +1,7 @@
-from app import socketio
+from flask_socketio import SocketIO, emit
+from app import app
+
+socketio = SocketIO(app)
 
 @socketio.on('update')
 def update_db(parameter):
--- app/static/functions.js
+++ app/static/functions.js
@@ -9,11 +9,6 @@ function random_bg_color() {
     }
 
 
-var socket = io.connect('http://' + document.domain + ':' + location.port);
-	socket.on('connect', function() {
-		socket.emit('my event', {data: 'I\'m connected!'});
-	});
-
-	function update(parameter) {
-		socket.emit('update', parameter)
-	}
+function update(parameter) {
+	socket.emit('update', parameter)
+}
